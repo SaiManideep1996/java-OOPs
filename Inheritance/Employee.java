1493981b40c3efc0226c7dc38339a34dc4132f67
SaiManideep1996 <pellimarisaimanideep@gmail.com>
2025-07-21 12:41:56 -0500

    Create Employee.java

---
+++ Inheritance/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    protected String name;
+    protected double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void displayInfo() {
+        System.out.println(name + " earns $" + salary);
+    }
+}
