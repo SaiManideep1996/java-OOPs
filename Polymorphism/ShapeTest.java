6b0548545537b0978dfd4addce29c38115bfb9ab
SaiManideep1996 <pellimarisaimanideep@gmail.com>
2025-07-21 14:19:33 -0500

    Create ShapeTest.java

---
+++ Polymorphism/ShapeTest.java
@@ -0,0 +1,9 @@
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape s1 = new Circle();
+        Shape s2 = new Rectangle();
+
+        s1.draw();
+        s2.draw();
+    }
+}
